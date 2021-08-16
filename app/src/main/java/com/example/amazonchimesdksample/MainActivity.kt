62db57d1b239d6da58ed4682b33d097678bcf3d7
BYAK <byakhee0314@gmail.com>
2021-08-16 18:35:03 +0900

    import chime library

--- app/src/main/java/com/example/amazonchimesdksample/MainActivity.kt
+++ app/src/main/java/com/example/amazonchimesdksample/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.amazonchimesdksample
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val PERMISSION_REQUEST_CODE = 1
+        val PERMISSIONS = arrayOf(
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA)
+
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE)
         setContentView(R.layout.activity_main)
     }
 }
